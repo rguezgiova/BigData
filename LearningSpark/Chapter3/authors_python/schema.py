88df043b2cedba32f0d98be6e48627ba0326e772
Giovanni <grodriguez.inf@gmail.com>
2022-04-28 17:25:32 +0200

    Subidos ultimos ejercicios

--- LearningSpark/Chapter3/authors_python/schema.py
+++
@@ -1,7 +0,0 @@
-from pyspark.sql.types import *
-
-schema = StructType([StructField("author", StringType(), False),
-                     StructField("title", StringType(), False),
-                     StructField("pages", IntegerType(), False)])
-
-schema2 = "author STRING, title String, pages INT"
